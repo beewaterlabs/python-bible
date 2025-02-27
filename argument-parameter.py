1da0ac7411ba967e09fa8225cad2cedce0f38d14
beewaterlabs <160707500+beewaterlabs@users.noreply.github.com>
2025-02-27 15:48:58 -0800

    Update argument-parameter.py

--- argument-parameter.py
+++ argument-parameter.py
@@ -6,16 +6,16 @@
 def greet(name):  # 'name' is a parameter
     print(f"Hello, {name}!")
 
-greet("Alice")  # 'Alice' is an argument
+greet("We are the Ramones, 1234!")  # 'We are the Ramones, 1234!' is an argument
 
 # Breakdown:
 # Function Definition: def greet(name):
     * name is the parameter. It's a placeholder for the value you will pass.
-# Function Call: greet("Alice")
-    * "Alice" is the argument. It's the actual value that gets passed to the function.
+# Function Call: greet("We are the Ramones, 1234!")
+    * "We are the Ramones, 1234!" is the argument. It's the actual value that gets passed to the function.
 So, parameters are used in the function definition, and arguments are used when you call the function.
 
 Script outout:
 
 ======= RESTART: /Users/baranda/Udemy/python_bible/Argument-Parameter.py =======
-Hello, Alice!
+Hello, We are the Ramones, 1234!!

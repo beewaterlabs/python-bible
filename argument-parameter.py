6f80d26142586d37338d7d5ed8c2bd9a7719c328
beewaterlabs <160707500+beewaterlabs@users.noreply.github.com>
2025-03-04 14:21:56 -0800

    Update argument-parameter.py

--- argument-parameter.py
+++ argument-parameter.py
@@ -10,12 +10,12 @@ greet("We are the Ramones, 1234!")  # 'We are the Ramones, 1234!' is an argument
 
 # Breakdown:
 # Function Definition: def greet(name):
-    * name is the parameter. It's a placeholder for the value you will pass.
+# name is the parameter. It's a placeholder for the value you will pass.
 # Function Call: greet("We are the Ramones, 1234!")
-    * "We are the Ramones, 1234!" is the argument. It's the actual value that gets passed to the function.
-So, parameters are used in the function definition, and arguments are used when you call the function.
+# "We are the Ramones, 1234!" is the argument. It's the actual value that gets passed to the function.
+# So, parameters are used in the function definition, and arguments are used when you call the function.
 
-Script outout:
+# Script outout:
 
-======= RESTART: /Users/baranda/Udemy/python_bible/Argument-Parameter.py =======
-Hello, We are the Ramones, 1234!!
+# ======= RESTART: /Users/baranda/Udemy/python_bible/Argument-Parameter.py =======
+# Hello, We are the Ramones, 1234!!

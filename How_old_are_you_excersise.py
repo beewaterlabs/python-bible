fa85d4fcae9b2ec5c3c72f018f17ad9dc0622628
beewaterlabs <160707500+beewaterlabs@users.noreply.github.com>
2025-04-10 02:10:56 -0700

    new file:   How_old_are_you_excersise.py 	modified:   hello_world.py 	modified:   hypotenuse_long_hand.py 	modified:   string_concatenation.py

---
+++ How_old_are_you_excersise.py
@@ -0,0 +1,21 @@
+# How old are you excersise:
+
+# First, create a variable called start, and set it equal to "I am ".
+# Remember the space after the word "am" !
+start = "I am "
+
+# Next, create a variable called age and set it equal to your age in years.
+# This must be a number
+age = 43
+
+# Next, create a variable called end, and set it equal to " years old".
+# Remember the space before the word "years"
+end = " years old"
+
+# Next, create a variable called output and use {} symbols and the format() function to stick the start, age and end variables
+# together to make a string.
+
+output = "{}{}{}".format(start, age, end)
+
+print(output)
+
--- string_concatenation.py
+++ string_concatenation.py
@@ -32,22 +32,5 @@ A + str(B)
 "{1} - {0}".format(A,B)
 '1 - part'
 
-# How old are you excersise:
 
-# First, create a variable called start, and set it equal to "I am ".
-# Remember the space after the word "am" !
-start = "I am "
-
-# Next, create a variable called age and set it equal to your age in years.
-# This must be a number
-age = 43
-
-# Next, create a variable called end, and set it equal to " years old".
-# Remember the space before the word "years"
-end = " years old"
-
-# Next, create a variable called output and use {} symbols and the format() function to stick the start, age and end variables
-# together to make a string.
-
-string = "{} {} {}"
 

517c8c6f4919be4daa7f2b1895caebf71778542e
beewaterlabs <160707500+beewaterlabs@users.noreply.github.com>
2025-04-10 02:29:36 -0700

    new file:   about.py

---
+++ about.py
@@ -0,0 +1,12 @@
+# Create a variable called name and set it equal to your name
+name = "My name is Brandon"
+# Create a variable called age and set it equal to your age
+age = 43
+
+# Create a variable called output, and use {} symbols and the format() function to 
+# make it contain a string like the example text in the description
+output = "{} and I am {} years old".format(name, age)
+
+# Finally, use the print() function to print the output.
+
+print(output)

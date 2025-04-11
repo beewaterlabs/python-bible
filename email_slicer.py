01e772da7f1afd7f068f172816b47bdb19cca9ae
beewaterlabs <160707500+beewaterlabs@users.noreply.github.com>
2025-04-10 22:44:46 -0700

    Adding email_slicer.py

---
+++ email_slicer.py
@@ -0,0 +1,21 @@
+# email_slicer.py
+
+# get user email address
+
+email = input("What is your email address?: ").strip()
+
+# slice out user nane
+
+user = email[:email.index("@")]
+
+# slice domain name:
+# Start at the character after the @ sign by using the index method:
+# email.index("@") + 1
+# and slice to the end of the string:
+
+domain = email[email.index("@") + 1:]
+
+# format message
+
+# display output message
+

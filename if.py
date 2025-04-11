42b08430bcfcadaac8da5c57d90df82e9b481630
beewaterlabs <160707500+beewaterlabs@users.noreply.github.com>
2025-04-11 03:37:00 -0700

    Adding: __pycache__/, if.py

---
+++ if.py
@@ -0,0 +1,10 @@
+num1 = 100
+num2 = 100
+
+if num1>num2:
+    print("num 1 is bigger than num 2")
+elif num2 > num1:
+    print("num 2 is bigger than num 1")
+else:
+    print("Both numbers are equal")
+

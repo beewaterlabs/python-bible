a61e9b29a164477caa56c8597fcbb2487b413b5d
beewaterlabs <160707500+beewaterlabs@users.noreply.github.com>
2025-04-12 01:47:56 -0700

    Adding: not_and.py

--- not_and.py
+++ not_and.py
@@ -19,7 +19,7 @@ if C >= 10 and D > 1:
 if not (C > 10 and D > 1):
     print("it worked")
 
-###
+### OR gate Table:
 # A and B: Represent the two input values (0 or 1, which correspond to False or True).
 # A OR B: Represents the output of the OR operation. 
 # The OR operation is true (1) if at least one of the inputs is true (1). 
@@ -36,3 +36,9 @@ if not (C > 10 and D > 1):
 # not gate only gives true if the condition is false:
 if not (C > 100 or D > 100):
     print("it worked")
+
+C = 6
+D = 2
+
+if (C > 5 and D > 5) or (C > 1 and D > 1):
+    print("it worked") 

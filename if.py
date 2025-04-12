e500e4238de620bb4766b2a899dfa8eea71c4991
beewaterlabs <160707500+beewaterlabs@users.noreply.github.com>
2025-04-12 00:55:31 -0700

    Adding: if.py

--- if.py
+++ if.py
@@ -7,6 +7,6 @@ if num1 > num2: # Replace CONDITION1 with a valid condition that makes this work
 elif num1 == num2: # Replace CONDITION2 with a valid condition that makes this work:
     print("num1 is equal to num2")
     
-elif num2 > num1: # Replace CONDITION3 with a valid condition that makes this work:
+elif num2 > num1: # Replace CONDITION3 with a valid condition that makes this work>
     print("num2 is bigger than num1")
     

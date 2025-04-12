107e91f31db89c5eb600fa58584a6b71fe7beee7
beewaterlabs <160707500+beewaterlabs@users.noreply.github.com>
2025-04-12 01:26:22 -0700

    Adding: not_and.py

--- not_and.py
+++ not_and.py
@@ -7,8 +7,13 @@ if not (C > 10 and D > 1):
     print("it worked")
 
 # And gate only gives true if both conditions are true:
-
 C =10
 D = 5
 if C >= 10 and D > 1:
     print("wholly molly batman, both conditions are true!")
+
+
+# not gate only gives true if the condition is false:
+if not (C > 10 and D > 1):
+    print("it worked")
+

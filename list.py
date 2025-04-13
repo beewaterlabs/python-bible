9ef128ae96ed4cf8587b6eb56f3bf7026f989c63
beewaterlabs <160707500+beewaterlabs@users.noreply.github.com>
2025-04-12 22:14:23 -0700

    Adding: list.py

--- list.py
+++ list.py
@@ -3,5 +3,31 @@
 our_list = [27, 46, -5, 17, 99]
 print(our_list)
 
+jackson = ["A", "B", "C", 1, 2, 3, "Do", "Rey", "Mi", True, False]
+
+print(jackson[4])
+
+print(jackson[7])
+
+print(jackson[-2])
+
+x = jackson[6]
+
+print(x)
+
+#jackson[start:end:step]
+
+jackson[0:3]
+print(jackson[0:3])
+
+jackson
+print(jackson)
+
+# position 2 is the third element:
+our_list = [1, 2, [3,4,5], 6, 7, 8]
+print(our_list[2])
+
+print(our_list[2][0])
+# 3
 
 
